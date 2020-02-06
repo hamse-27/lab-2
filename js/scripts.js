c92b0759545c102cfc2f7440f651c20ac18be712
Hamse Ahmed <P17151445@LEC-ADMIN.dmu.ac.uk>
2020-02-06 11:18:46 +0000

    my first commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+const hello = document.getElementById("paragraphID");
+
+hello.addEventListener("click", greeting);
+
+
+function greeting() {
+p1 = console.log("Hello!");
+hello.textContent="This is the new text."
+return p1;
+}
